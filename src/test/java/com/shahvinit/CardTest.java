fa1b05ba440ecebb340bd09cda91251ec12f1334
Vinit Shah <vinitshah@salesforce.com>
2020-08-18 22:59:24 -0700

    Initial Commit: Setup

---
+++ src/test/java/com/shahvinit/CardTest.java
@@ -0,0 +1,13 @@
+package com.shahvinit;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CardTest {
+
+    @Test
+    public void shouldPass() {
+         assertThat(true)
+                 .isTrue();
+    }
+}
